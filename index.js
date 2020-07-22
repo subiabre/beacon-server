24f77cc31dfcd37693c37b427052ada5197de5e3
Facu <subiabrewd@gmail.com>
2020-07-22 02:22:58 +0200

    Ignore everything memory related

---
+++ index.js
@@ -0,0 +1,64 @@
+"use strict";
+
+// Express
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Socket.io
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+// Sequelize
+const db = require('./src/database').connection();
+db.sync({force: true, logging: false});
+
+const ip = require('ip');
+
+const ConsoleString = require('./src/console-string');
+const Song = require('./src/model/song');
+const ConsoleColors = ConsoleString.colors
+
+app.get('/songs', async (req, res) => {
+    const songs = await Song.findAll();
+
+    res.send({songs});
+});
+
+const listener = http.listen(process.env.PORT || 3127, async () => {
+    new ConsoleString(`beacon-server now live.\n`)
+        .log();
+
+    try {
+        await db.authenticate();
+        new ConsoleString(`Using local database.`)
+            .tab(2)
+            .color(ConsoleColors.FgMagenta)
+            .log();
+    } catch (error) {
+        new ConsoleString(`Unable to connect to the local database: ${error}`)
+            .tab(2)
+            .color(ConsoleColors.FgRed)
+            .log();
+    }
+
+    let netAddress = new ConsoleString(`http://${ip.address()}:${listener.address().port}`)
+        .color(ConsoleColors.FgCyan)
+        .string;
+
+    let localAddress = new ConsoleString(`http://localhost:${listener.address().port}`)
+        .color(ConsoleColors.FgCyan)
+        .tab(6)
+        .string;
+
+    new ConsoleString(`On your machine: ${localAddress}`)
+        .tab(2)
+        .log();
+
+    new ConsoleString(`On your local network: ${netAddress}`)
+        .tab(2)
+        .log();
+});
