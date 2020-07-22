85b0fd35285dcb82cc13c02eb89d9030bf3e6320
Facu <subiabrewd@gmail.com>
2020-07-22 22:57:14 +0200

    Add SocketList component

---
+++ client/src/components/SocketList.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import Socket from 'socket.io-client';
+const socket = Socket();
+
+class SocketList extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+
+        this.state = {
+            sockets: []
+        };
+    }
+
+    componentDidMount()
+    {
+        socket.on('socket:update', (sockets) => {
+            this.setState({
+                sockets: sockets
+            });
+        });
+    }
+
+    socketLi(li)
+    {
+        const id = socket.id;
+        
+        if (li.id === id) {
+            return (
+                <li
+                    title = 'Current connection'
+                    className = 'Socket-Current'
+                    key = {li.id}
+                >
+                    {li.userAgent.name} @ {li.userAgent.os} {li.userAgent.device_type}
+                </li>  
+            );
+        }
+
+        return (
+            <li
+                title = {li.id}
+                className = 'Socket'
+                key = {li.id}
+            >
+                {li.userAgent.name} @ {li.userAgent.os} {li.userAgent.device_type}
+            </li>
+        );
+    }
+
+    render()
+    {
+        const sockets = this.state.sockets;
+
+        return (
+            <ul className = "SocketList">
+                {sockets.map((socket) => this.socketLi(socket))}
+            </ul>
+        );
+    }
+}
+
+export default SocketList;
