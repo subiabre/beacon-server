180e059d4440e9aedc9440e75b062b3b594bc4aa
Facu <subiabrewd@gmail.com>
2020-07-23 02:57:44 +0200

    Refactor routes to be more flexible

--- src/routes.js
+++ src/routes.js
@@ -9,76 +9,96 @@ router.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/index.html');
 });
 
-router.get('/songs', async (req, res) => {
-    const songs = await Song.findAll();
-
-    res.send({songs});
-});
-
-router.get('/songs/:title', async (req, res) => {
+router.get('/songs/:by/:name', async (req, res) => {
+    const byParam = req.params.by;
     const songs = await Song.findAll({
         where: {
-            name: {
-                [Op.like]: req.params.title
+            [byParam]: {
+                [Op.or]: {
+                    [Op.like]: req.params.name,
+                    [Op.startsWith]: req.params.name,
+                    [Op.eq]: req.params.name
+                }
             }
         }
     });
 
-    res.send({songs});
-});
+    if (songs.length > 0) {
+        res.send(songs);
+    }
 
-router.get('/image/:id', async (req, res) => {
-    const song = await Song.findAll({
-        where: {
-            id: req.params.id
-        }
-    });
-
-    let image = Buffer.from(song[0].image, 'binary');
-
-    res.setHeader('Content-Type', 'image/jpeg');
-    res.send(image);
+    res.sendStatus(404);
 });
 
 router.get('/release/:name', async (req, res) => {
     const songs = await Song.findAll({
         where: {
-            release: {
-                [Op.or]: {
-                    [Op.like]: req.params.name,
-                    [Op.startsWith]: req.params.name,
-                    [Op.eq]: req.params.name
-                }
-            }
+            release: req.params.name
         }
     });
 
-    const data = songs[0];
+    if (songs.length > 0) {
+        let data = songs[0];
+        let release = {
+            name: data.release,
+            artist: data.artist,
+            image: '/image/' + data.id,
+            songs: songs
+        }
 
-    const release = {
-        name: data.release,
-        artist: data.artist,
-        image: data.image,
-        songs: songs,
+        res.send(release);
     }
 
-    res.send(release);
+    res.sendStatus(404);
 });
 
 router.get('/artist/:name', async (req, res) => {
     const songs = await Song.findAll({
         where: {
-            artist: {
-                [Op.or]: {
-                    [Op.like]: req.params.name,
-                    [Op.startsWith]: req.params.name,
-                    [Op.eq]: req.params.name
-                }
-            }
+            artist: req.params.name
         }
     });
 
-    res.send({songs});
+    if (songs.length > 0) {
+        let data = songs[0];
+        let dataReleases = [];
+        
+        songs.forEach((song, index) => {
+            // Push to releases if release is not in array
+            if (dataReleases.indexOf(song.release) < 0) {
+                dataReleases.push(song.release)
+            }
+
+            // End of loop
+            if (index + 1 == songs.length) {
+                let artist = {
+                    name: data.artist,
+                    releases: dataReleases
+                }
+    
+                res.send(artist);
+            }
+        });
+    }
+
+    res.sendStatus(404);
+});
+
+router.get('/image/:id', async (req, res) => {
+    const song = await Song.findOne({
+        where: {
+            id: req.params.id
+        }
+    });
+
+    if (!song) {
+        res.sendStatus(404);
+    }
+
+    let image = Buffer.from(song[0].image, 'binary');
+
+    res.setHeader('Content-Type', 'image/jpeg');
+    res.send(image);
 });
 
 module.exports = router;
